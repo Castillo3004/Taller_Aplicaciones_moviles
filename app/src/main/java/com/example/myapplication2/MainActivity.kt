b67e6a792f1ceceed47ab3225be8af9254de54ec
Castillo3004 <cacastillo34@utpl.edu.ec>
2022-10-28 02:03:51 -0500

    Finalizado

--- app/src/main/java/com/example/myapplication2/MainActivity.kt
+++ app/src/main/java/com/example/myapplication2/MainActivity.kt
@@ -11,6 +11,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         MayoroMenor()
         TablaMultiplicar()
+        listadoParalelo()
+        Vehiculo()
+        ValidaCedula ()
+        IVA()
     }
 
     // Validar si una persona es mayor o menor de edad
@@ -22,6 +26,8 @@ class MainActivity : AppCompatActivity() {
             println("Es menor de edad")
         }
     }
+
+    // Presentar la tabla de multiplicar de n numero de forma ascendente y descendente.
 
     fun TablaMultiplicar(){
 
@@ -42,11 +48,89 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun listadoParalelo(){
+    // Mostrar el listado de paralelo de la materia de Plataformas móviles y los subgrupos por proyectos (Mostrar los resultados ordenados).
+    fun listadoParalelo() {
+        println("Estudiantes")
+        val estudiantes = listOf<String>(
+            "Carlos Castillo",
+            "Joselo Ordoñez",
+            "Isaias Silva",
+            "Anthonny Espinosa"
+        )
 
-        val myArray = arrayListOf<String>()
+        var lista = estudiantes.sorted()
+        println(lista)
+
+        println("\nGRUPOS")
+        val grupos = mutableMapOf(
+            "Carlos Castillo" to "G1",
+            "Joselo Ordoñez" to "G1",
+            "Isaias Sila" to "G2",
+            "Anthonny Espinosa" to "G2"
+        )
+        var listaGrupos = grupos.toList().sortedBy { (_, value) -> value}.toMap()
+        for(aux in listaGrupos){
+            println(aux.key + " - "+ aux.value)
+        }
 
     }
+
+
+    // Presentar las propiedades de un vehículo utilizando clases, como tracción, motor, tipo de vehículo, capacidad
+    fun Vehiculo (){
+        val primero = CaracteristicasVehiculo(traccion = "Automatico", motor = 4000, tipo = "SEDAN" , capacidad = 4)
+        println("\nTraccion: "+primero.traccion +"\nMotor: "+ primero.motor)
+    }
+
+    class CaracteristicasVehiculo( val traccion:String, val motor:Int, val tipo:String, val capacidad:Int){
+
+    }
+
+    // Cedula con algoritmo
+
+    fun ValidaCedula (){
+        val lis1 = mutableListOf(2,1,2,1,2,1,2,1,2)
+        var lis2 = mutableListOf(2,1,2,1,2,1,2,1,2)
+        var cedula = "1105140881"
+        var cont = 0
+        val aux = cedula.map(Character::getNumericValue).toIntArray()
+        for(i in aux){
+            if(cont < aux.size - 1 ){
+                var mul = i * lis1[cont]
+                if(mul.toString().length > 1){
+                    val aux2 = mul.toString().map(Character::getNumericValue).toIntArray()
+                    mul = 0
+                    for ( i in aux2){
+                        mul += i
+                    }
+                }
+                lis2[cont] = mul
+                cont += 1
+            }
+        }
+        var comprobacion = aux.sum() - lis2.sum()
+        if(comprobacion == 0){
+            println("La Cedula es valida")
+        }else{
+            println("La Cedula es invalida")
+        }
+
+
+    }
+
+    // IVA
+    fun IVA(){
+        val numero = 25
+        val iva = (numero*0.12)
+        val res = (numero + iva)
+        println("Total sin IVA =  "+ numero)
+        println("IVA = " +  iva)
+        println("Total + IVA = "+ res)
+    }
+
+
+
+
 
 }
 
